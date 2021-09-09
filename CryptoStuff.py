ba31f590a54bad0fefef2d0bd84de801159c9691
PythonBot0 <90237430+PythonBot0@users.noreply.github.com>
2021-09-09 17:05:24 +1000

    Add files via upload

--- CryptoStuff.py
+++ CryptoStuff.py
@@ -18,3 +18,21 @@ webpage3 = webpage_in3.content
 htmldataforwebpage3 = BeautifulSoup(webpage_in3.content, "html.parser")
 current_cardano_price = htmldataforwebpage3.td
 print(str(current_cardano_price) + " is the current Cardano price (in USD)")
+
+webpage_in4 = requests.get("https://coinmarketcap.com/currencies/binance-coin/")
+webpage4 = webpage_in4.content
+htmldataforwebpage4 = BeautifulSoup(webpage_in4.content, "html.parser")
+current_binancecoin_price = htmldataforwebpage4.td
+print(str(current_binancecoin_price) + " is the current Binance Coin price (in USD)")
+
+webpage_in5 = requests.get("https://coinmarketcap.com/currencies/tether/")
+webpage5 = webpage_in5.content
+htmldataforwebpage5 = BeautifulSoup(webpage_in5.content, "html.parser")
+current_tether_price = htmldataforwebpage5.td
+print(str(current_tether_price) + " is the current Tether price (in USD)")
+
+webpage_in6 = requests.get("https://coinmarketcap.com/currencies/solana/")
+webpage6 = webpage_in6.content
+htmldataforwebpage6 = BeautifulSoup(webpage_in6.content, "html.parser")
+current_solana_price = htmldataforwebpage6.td
+print(str(current_solana_price) + " is the current Solana price (in USD)")
